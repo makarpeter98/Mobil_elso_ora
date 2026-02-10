40bd4d3fb03e4a6867d3c3ad24811bf87a85e3ed
makar.peter98 <makar.peter98@gmail.com>
2026-02-10 16:39:49 +0100

    Gomb a felületen

--- app/src/main/java/hu/unideb/mobil/mobil_elso_ora/MainActivity.java
+++ app/src/main/java/hu/unideb/mobil/mobil_elso_ora/MainActivity.java
@@ -1,6 +1,8 @@
 package hu.unideb.mobil.mobil_elso_ora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView myTextView;
+    Integer counter = 0;
     //teszt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        myTextView = findViewById(R.id.textView);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void PressMeButtonOnClick(View view) {
+        myTextView.setText(counter.toString());
+        counter++;
     }
 }
